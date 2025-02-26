fccea1943630ab756b30e6d8ed7bb972a93e4451
Gabriel Henrique Coelho Freitas <94490310+03gabriel-coelho@users.noreply.github.com>
2025-02-26 11:06:00 -0300

    feat(web): atualiza estilo do Header, adiciona ícone de prateleira e ajusta variáveis de cor

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import styles from "@/styles/Header.module.css";
+import { GiBookshelf } from "react-icons/gi";
 
 export default function Header() {
   return (
     <header className={styles.header}>
+      <GiBookshelf width={200} height={200}/>
       <h1>Zievo Books</h1>
     </header>
   );
